09e4490df453a85e6eff1972deaad8a38a730d6c
Mayur <mayurk.yadav2023@gmail.com>
2025-02-03 17:42:00 -0500

    Add files via upload

---
+++ ECommerceApplicatonTesting/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceApplication;
+using NUnit.Framework;
+
+namespace ECommerceApplicatonTesting
+{
+    public class ProductTests
+    {
+        [Test]
+        public void ProductID_ValidValue_SetsCorrectly()
+        {
+            // arrange
+            int prodID = 500;
+            string prodName = "Laptop";
+            decimal itemPrice = 1200;
+            int stockAmount = 50;
+
+            // act
+            var product = new Product(prodID, prodName, itemPrice, stockAmount);
+
+            // assert
+            Assert.That(product.ProdID, Is.EqualTo(prodID));
+        }
+    }
+}
